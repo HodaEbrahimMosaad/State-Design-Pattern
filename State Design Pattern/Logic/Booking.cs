54b5f98d9e34ffada768e49f8571d75d1403a9c3
HodaEbrahimMosaad <58063429+HodaEbrahimMosaad@users.noreply.github.com>
2021-07-16 20:51:43 +0200

    Add project files.

---
+++ State Design Pattern/Logic/Booking.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using State_Design_Pattern.UI;
+
+namespace State_Design_Pattern.Logic
+{
+    public class Booking
+    {
+        private MainWindow View { get;  set; }
+        public string Attendee { get; set; }
+        public int TicketCount { get; set; }
+        public int BookingID { get; set; }
+
+        private CancellationTokenSource cancelToken;
+       
+        public Booking(MainWindow view)
+        {
+            View = view;
+        }
+
+        public void SubmitDetails(string attendee, int ticketCount)
+        {
+           
+        }
+
+        public void Cancel()
+        {
+           
+        }
+
+        public void DatePassed()
+        {
+
+        }
+
+        public void ProcessingComplete(Booking booking, ProcessingResult result)
+        {
+            switch (result)
+            {
+                case ProcessingResult.Sucess:
+                    ShowState("Booked");
+                    View.ShowStatusPage("Enjoy the Event");
+                    break;
+                case ProcessingResult.Fail:
+                    View.ShowProcessingError();
+                    Attendee = string.Empty;
+                    BookingID = new Random().Next();
+                    ShowState("New");
+                    View.ShowEntryPage();
+                    break;
+                case ProcessingResult.Cancel:
+                    ShowState("Closed");
+                    View.ShowStatusPage("Processing Canceled");
+                    break;
+            }
+        }
+
+        public void ShowState(string stateName)
+        {
+            View.grdDetails.Visibility = System.Windows.Visibility.Visible;
+            View.lblCurrentState.Content = stateName;
+            View.lblTicketCount.Content = TicketCount;
+            View.lblAttendee.Content = Attendee;
+            View.lblBookingID.Content = BookingID;
+        }
+
+      
+
+    }
+}
+
+
